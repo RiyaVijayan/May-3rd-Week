34261bad511bf56ea34663af1831c61db9e361fe
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-25 14:59:18 +0530

    Add files via upload

---
+++ packagetwo1.java
@@ -0,0 +1,13 @@
+//importing using packagename.classname
+
+package mypack;  
+import pack.packageone;  
+  
+class packagetwo1
+{  
+  public static void main(String args[])
+  {  
+   packageone obj = new packageone();  
+   obj.msg();  
+  }  
+}  
