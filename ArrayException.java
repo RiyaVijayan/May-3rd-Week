58798b04ab81b720b797f6a348b99eea1805951c
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-25 17:56:40 +0530

    Add files via upload

---
+++ ArrayException.java
@@ -0,0 +1,13 @@
+// Program to demonstrate the Exception in a Java Array
+  
+public class ArrayException
+{  
+	public static void main(String args[])
+	{  
+		int arr[]={50,60,70,80};  
+		for(int i=0;i<=arr.length;i++)
+		{  
+			System.out.println(arr[i]);  
+		}  
+	}
+} 
