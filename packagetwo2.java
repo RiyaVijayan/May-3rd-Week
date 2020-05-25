6ba3e83b981211d398bfc0f8b2afe99e3fede079
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-25 15:53:42 +0530

    Add files via upload

---
+++ packagetwo2.java
@@ -0,0 +1,12 @@
+//inserting package using fully qualified name
+
+package mypack;
+  
+class packagetwo2
+{
+  public static void main(String args[])
+  {  
+   pack.packageone obj = new pack.packageone();//using fully qualified name  
+   obj.msg();  
+  }  
+}  
