9359a35bc11f6de1ea910899cc82d0f3f6a511c8
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-26 17:37:51 +0530

    Add files via upload

---
+++ Exception2.java
@@ -0,0 +1,19 @@
+//Java Program to show Exception with custom messages
+public class Exception2
+{  
+
+	public static void main(String[] args) 
+	{  
+		try  
+		{  
+			int data=50/0;    
+		}  
+
+		catch(Exception e)  
+		{  
+
+			System.out.println("Can't divided by zero");  
+		}  
+	}  
+
+}  
